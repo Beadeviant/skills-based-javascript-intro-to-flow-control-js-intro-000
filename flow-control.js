dc65b96cc3ef6c99ed7563dfc65d059e4965b987
lite-closure-2800 <bri.barden@gmail.com>
2019-11-03 19:21:10 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -32,25 +32,25 @@ return age <= 19 && age >= 13 ? "You are a teenager" : "You are not a teenager"
 function switchAge(age) {
 switch (age) {
   case 13:
-    console.log("You are a teenager");
+    return console.log("You are a teenager");
     break;
     case 14:
-      console.log("You are a teenager");
+      return console.log("You are a teenager");
       break;
     case 15:
-        console.log("You are a teenager");
+        return console.log("You are a teenager");
         break;
     case 16:
-          console.log("You are a teenager");
+          return console.log("You are a teenager");
           break;
     case 17:
-    console.log("You are a teenager");
+  return console.log("You are a teenager");
     break;
     case 18:
-      console.log("You are a teenager");
+    return console.log("You are a teenager");
       break;
       case 19:
-        console.log("You are a teenager");
+      return  console.log("You are a teenager");
         break;
   default:
 return "You have an age";
